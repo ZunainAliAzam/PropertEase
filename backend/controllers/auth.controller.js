147c8292effa9b7e38f62aee4762beb386b93c2c
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-06 04:30:10 +0500

    Registration of new user completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,24 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
-export const register = async(req, res) => {
+export const register = async (req, res) => {
   const { username, email, password } = req.body;
   //hash the password
-  const hashPassword = await bcrypt.hash(password,10);
-  
-  //create a new account and save it in the database
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //create a new account and save it in the database
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashPassword,
+      },
+    });
+    res.status(201).send({ message: "New user created successfully" });
+  } catch (err) {
+    res.status(500).send({ message: "Failed to create new user" });
+  }
 };
 export const login = (req, res) => {
   //DB operations
