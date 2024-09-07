f4ea51aa65ebef1a9373f5623c3df22207398064
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-08 04:29:04 +0500

    main bg set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar.jsx'
+import React from "react";
+import Navbar from "./components/navbar/Navbar.jsx";
+import HomePage from "./routes/homepage/homePage.jsx";
 import "./layout.scss";
 
 const App = () => {
   return (
-    <div className='layout'>
-      <Navbar />
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/routes/homepage/homePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./homePage.scss";
+const homePage = () => {
+  return (
+    <div className='homePage'>
+      <div className="textContainer">text</div>
+      <div className="imgContainer">
+        <img src="./bg.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default homePage
