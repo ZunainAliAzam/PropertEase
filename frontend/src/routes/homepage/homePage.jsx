ead094f611abfd6903615ea0b7eef9e181e2b723
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-08 04:34:44 +0500

    commit: main section completed

--- frontend/src/routes/homepage/homePage.jsx
+++ frontend/src/routes/homepage/homePage.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 const homePage = () => {
   return (
-    <div className='homePage'>
-      <div className="textContainer">text</div>
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="imgContainer">
-        <img src="./bg.png" alt="" />
+        <img src="/bg.png" alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default homePage
+export default homePage;
