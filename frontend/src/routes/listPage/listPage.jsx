eadb70fcec0b06c6ca03dab01bb9594b5e5c1f31
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-12 02:01:37 +0500

    commit: creating filter functionality of properties

--- frontend/src/routes/listPage/listPage.jsx
+++ frontend/src/routes/listPage/listPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./listPage.scss";
+import { listData } from "../../lib/dummyData";
 
 const ListPage = () => {
+  const data = listData;
   return (
-    <div></div>
+    <div className="listPage">
+      <div className="list">
+        <div className="wrapper">
+          
+        </div>
+      </div>
+      <div className="map">Map container</div>
+    </div>
   );
 };
 
