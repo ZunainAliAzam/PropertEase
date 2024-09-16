258df0e11fbe07540ab7315a64b6d440cf88c424
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-16 23:54:29 +0500

    commit: Card UI styling underway

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -7,7 +7,36 @@ const Card = ({ item }) => {
       <Link to={`${item.id}`} className="imgContainer">
         <img src={item.images} alt="" />
       </Link>
-      <div className="textContainer"></div>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">${item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="./bed.png" alt="" />
+              <span>{item.bedroom}</span>
+            </div>
+            <div className="feature">
+              <img src="./bath.png" alt="" />
+              <span>{item.bathroom}</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
