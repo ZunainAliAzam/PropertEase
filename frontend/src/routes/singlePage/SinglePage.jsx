c447474929b571d3ab728e3fd713e85eb01a488d
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-21 01:36:23 +0500

    commit: designed details section

--- frontend/src/routes/singlePage/SinglePage.jsx
+++ frontend/src/routes/singlePage/SinglePage.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import "./singlePage.scss"
-import Slider from '../../components/slider/Slider'
+import React from "react";
+import "./singlePage.scss";
+import Slider from "../../components/slider/Slider";
+import { listData } from "../../lib/dummyData";
 
 const SinglePage = () => {
   return (
-    <div className='singlePage'>
+    <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider/>
+          <Slider />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>apartment 1</h1>
+                <div className="address">
+                  <img src="./pin.png" alt="" />
+                  <p>1533 Rixon Way</p>
+                </div>
+                <div className="price">$1200</div>
+              </div>
+              <div className="user">
+                <img src="./person.png" alt="" />
+                <span>John Doe</span>
+              </div>
+            </div>
+            <div className="bottom">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea
+              placeat odio porro, ullam minus unde in, aut corporis illo magni
+              repellat corrupti eos dolorem et incidunt quos alias ipsam
+              architecto?
+            </div>
+          </div>
         </div>
       </div>
       <div className="features">
         <div className="wrapper"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePage
+export default SinglePage;
