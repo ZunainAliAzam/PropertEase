d023a053d4a03d28724adf0267665f1551f64f0c
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-20 23:51:10 +0500

    commit: designing individual property pages

--- frontend/src/routes/singlePage/SinglePage.jsx
+++ frontend/src/routes/singlePage/SinglePage.jsx
@@ -4,7 +4,12 @@ import "./singlePage.scss"
 const SinglePage = () => {
   return (
     <div className='singlePage'>
-      
+      <div className="details">
+        <div className="wrapper">dsd</div>
+      </div>
+      <div className="features">
+        <div className="wrapper">sdsd</div>
+      </div>
     </div>
   )
 }
