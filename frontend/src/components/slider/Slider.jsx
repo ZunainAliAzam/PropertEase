de1af0f9165d3625cb6abb3b848123cbc8a865b6
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-28 03:57:57 +0500

    commit: implementing image slider component

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./slider.scss";
 
-const Slider = ({images}) => {
+const Slider = ({ images }) => {
+  const [setImage, setImageIndex] = useState(null);
+
   return (
-    <div className='slider'>
+    <div className="slider">
       <div className="bigImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImage">
         {images.slice(1).map((image, index) => (
-          <img key={index} src={image} alt="" />
+          <img
+            key={index}
+            src={image}
+            alt=""
+            onClick={setImageIndex(index[index + 1])}
+          />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
