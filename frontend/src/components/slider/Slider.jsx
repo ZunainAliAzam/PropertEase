cd543298ad8fdd5a24ea9d511355ff67ff481931
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-27 23:40:00 +0500

    commit: implementing image slider component

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./slider.scss";
 
-const Slider = () => {
+const Slider = ({images}) => {
   return (
     <div className='slider'>
-      
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImage">
+        {images.slice(1).map((image, index) => (
+          <img key={index} src={image} alt="" />
+        ))}
+      </div>
     </div>
   )
 }
