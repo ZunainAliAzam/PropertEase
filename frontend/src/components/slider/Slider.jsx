7fcc6a94fc109c76714c1f057daa9dc692dac501
ZunainAliAzam <zunainazam456@gmail.com>
2024-09-28 05:14:57 +0500

    commit: implementing image slider component

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -4,8 +4,30 @@ import "./slider.scss";
 const Slider = ({ images }) => {
   const [imageIndex, setImageIndex] = useState(null);
 
+  const changeSlide = () => {
+    
+  }
   return (
     <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide("left")}>
+            <img src="./arrow.png" alt="" />
+          </div>
+
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+
+          <div className="arrow" onClick={() => changeSlide("right")}>
+            <img src="./arrow.png" alt="" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
+
       <div className="bigImage">
         <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
