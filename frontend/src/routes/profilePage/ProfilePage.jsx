0dbd674a8cb76857ce189549beac21b2de8ac8d8
ZunainAliAzam <zunainazam456@gmail.com>
2024-10-21 00:14:53 +0500

    commit: Creating Profile Page

--- frontend/src/routes/profilePage/ProfilePage.jsx
+++ frontend/src/routes/profilePage/ProfilePage.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React from "react";
 import "./profilePage.scss";
 
 const ProfilePage = () => {
   return (
-    <div className='profilePage'>
+    <div className="profilePage">
       <div className="details">
         <div className="wrapper">
-          <div className="title"></div>
-          <div className="info"></div>
-          <div className="title"></div>
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              <span><b>John Doe</b></span>
+              <p>Email: john.doe@example.com</p>
+              <p>Phone: 123-456-7890</p>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
         </div>
       </div>
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
