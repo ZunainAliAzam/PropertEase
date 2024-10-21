550ba77ff091384be45ff2bfe1e04a528f94c8a6
ZunainAliAzam <zunainazam456@gmail.com>
2024-10-21 17:05:18 +0500

    commit: Chat feature added

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.scss";
 const Chat = () => {
+  const [chat, setChat] = useState(true)
   return (
     <div className="chat">
       <div className="messages">
@@ -54,7 +55,48 @@ const Chat = () => {
           <p className="text">Lorem ipsum dolor sit amet...</p>
         </div>
       </div>
-      <div className="chatbox"></div>
+      {chat &&(<div className="chatbox">
+        <div className="top">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            John Doe
+          </div>
+          <span className="close" onClick={()=>setChat(null)}>X</span>
+        </div>
+        <div className="center">
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <span>1 hour  ago</span>
+          </div>
+        </div>
+        <div className="bottom">
+          <textarea></textarea>
+          <button>Send</button>
+        </div>
+      </div>)}
     </div>
   );
 };
