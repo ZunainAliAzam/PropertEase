7e70fd9e353a4f0f7ac33b1ac75377b56200602a
ZunainAliAzam <zunainazam456@gmail.com>
2024-12-14 03:11:50 +0500

    commit: get one user api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,24 +2,43 @@ import prisma from "../lib/prisma.js";
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await prisma.user.findMany()
-    res.status(200).json(users);
+    const users = await prisma.user.findMany();
+    return res.status(200).json(users);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "failed to get all users!" });
+    return res.status(500).json({ message: "failed to get all users!" });
   }
 };
 
 export const getUser = async (req, res) => {
   try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.params.id },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json(user);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "failed to get user!" });
+    return res.status(500).json({ message: "failed to get user!" });
   }
 };
 
 export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+  const body = req.body;
+
+  if (id !== tokenUserId) {
+    res.status(403).json({ message: "user unauthorized" });
+  }
   try {
+    const updateUser = await prisma.user.update({
+      where: { id },
+      data: body,
+    });
+    return res.status(200).json(updateUser);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "failed to update user!" });
--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
@@ -7,12 +7,12 @@ export const verifyToken = (req, res, next) => {
     return res.status(401).json({ message: "You are not logged in." });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid token." })
-    }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userId = payload.userId;
-
     next();
-  });
+  } catch (err) {
+    console.error("Token verification error:", err);
+    return res.status(403).json({ message: "Invalid token." });
+  }
 };
