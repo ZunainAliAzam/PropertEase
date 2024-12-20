cecf859107aab2e4007018bff59445b2e7293a46
ZunainAliAzam <zunainazam456@gmail.com>
2024-12-20 23:53:39 +0500

    commit: get all post API added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,37 +1,58 @@
-const getPosts = async (req, res) => {
+import prisma from "../lib/prisma.js";
+
+export const getPosts = async (req, res) => {
   try {
+    const posts = await prisma.post.findMany();
+    return res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Failed to get posts!" });
   }
 };
-const getPost = async (req, res) => {
+export const getPost = async (req, res) => {
+  const id = req.params.id;
   try {
+    const onePost = await prisma.post.findUnique({
+      where: { id },
+    });
+    return res.status(200).json(onePost);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Failed to get post!" });
   }
 };
-const addPost = async (req, res) => {
+export const addPost = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+  const body = req.body;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: "Unauthorized user!" });
+  }
   try {
+    const addPost = await prisma.post.create({
+      where: { id },
+      data: {
+        ...body,
+      },
+    });
+    return res.status(201).json(addPost);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Failed to add post!" });
   }
 };
-const updatePost = async (req, res) => {
+export const updatePost = async (req, res) => {
   try {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Failed to update post!" });
   }
 };
-const deletePost = async (req, res) => {
+export const deletePost = async (req, res) => {
   try {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Failed to delete post!" });
   }
 };
-
-export default { getPosts, getPost, addPost, updatePost, deletePost };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -6,13 +6,14 @@ import {
   updatePost,
   deletePost,
 } from "../controllers/post.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js"
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.post("/", addPost);
-router.put("/:id", updatePost);
-router.delete("/:id", deletePost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
